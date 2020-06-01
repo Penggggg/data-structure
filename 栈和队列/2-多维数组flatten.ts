b4743bbb99f15450c8468d2b07fd7464438b75d4
何卓鹏 <hezhuopeng@cvte.com>
2020-06-01 11:41:53 +0800

    .

---
+++ 栈和队列/2-多维数组flatten.ts
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @description
+ * 题目：
+ * 
+ * [1, [2, [3, [4, 5]]], 6] -> [1, 2, 3, 4, 5, 6]
+ */
+
+
+/**
+ * 
+ * @description
+ * 方法1:：最小功能拆解 + 递归
+ */
+const flatten = ( nest: any[ ]): any[ ] => {
+    return nest.reduce(( cur, pre ) => (
+        pre.concat( Array.isArray( cur ) ? flatten( cur ) : pre )
+    ), [ ]);
+}
